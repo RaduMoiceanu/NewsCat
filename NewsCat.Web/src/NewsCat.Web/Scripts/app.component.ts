b732e18795881d1d7ff5bf0b61fb907d58e44f8b
Radu Moiceanu <radu.moiceanu@gmail.com>
2016-07-17 22:43:42 +0300

    Having trouble with Angular 2 calling RxJS and getting 404s...

---
+++ NewsCat.Web/src/NewsCat.Web/Scripts/app.component.ts
@@ -0,0 +1,13 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'news-cat-app',
+    template: '<p>{​{Title}​}</p>'
+})
+export class AppComponent {
+    Title: string;
+
+    constructor() {
+        this.Title = 'Hello World';
+    }
+}
